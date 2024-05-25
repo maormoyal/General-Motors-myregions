7d3a222c9ebac5feb707933554cd5b061bcf6c6d
maor moyal <maorkab@gmail.com>
2024-05-25 22:46:27 +0300

    Update Vite config for API proxying

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxying requests for images
       '/api': {
         target: 'https://myregions.azurewebsites.net',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\//, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
